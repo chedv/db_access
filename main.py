4621eb4fbbd86b4b924a29829c35419a5ead9512
chedv <chernovdv001@gmail.com>
2021-02-11 20:37:52 +0200

    handle non-unique datetime objects

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def extend_with_end_time(series):
 
 def check_mistakes_in_sessions(series):
     for current_start, end in zip(series.session_start, series.session_end):
+        if len(series.session_start) != len(set(series.session_start)):
+            return False
         actual_starts = [start for start in series.session_start if start > current_start]
         if not actual_starts:
             return True
